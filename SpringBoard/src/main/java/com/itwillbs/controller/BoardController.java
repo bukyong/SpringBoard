94b8de3e88a98eccb4ea43826daaa886d429619c
bukyong <pbg736837@gmail.com>
2024-02-21 15:23:57 +0900

    Feat : /board/register(GET) 작성완료

---
+++ SpringBoard/src/main/java/com/itwillbs/controller/BoardController.java
@@ -0,0 +1,23 @@
+package com.itwillbs.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+@RequestMapping(value = "/board/*")
+public class BoardController {
+
+	private static final Logger logger = LoggerFactory.getLogger(BoardController.class);
+
+	// 글쓰기 GET : /board/register
+	@RequestMapping(value = "/register", method = RequestMethod.GET)
+	public void registerGET() throws Exception {
+		logger.debug("/board/register -> registerGET() 호출");
+		logger.debug("/board/register.jsp 뷰 연결");
+		
+	}
+	
+}
